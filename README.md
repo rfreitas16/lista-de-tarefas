43a07ce03a249d5158742193b4a1877de5b31902
rfreitas16 <rfreitas16@hotmail.com>
2025-02-03 14:38:14 -0300

    criando os components

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-#Lista de tarefas simples em react
-
-![Lista de tarefas](https://www.flickr.com/photos/202207952@N07/shares/g87ms01MNv)
+#Lista de tarefas simples em react com salvamento em localStorange.
--- src/components/main.js
+++ src/components/main.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-import './main.css';
-import {FaPlus} from 'react-icons/fa';
 
-import {FaEdit, FaWindowClose} from 'react-icons/fa';
+import Form from './Form';
+import Tarefas from './Tarefas';
+import './main.css';
+
+
 
 export default class Main extends Component{
     state = {
@@ -68,7 +70,7 @@ export default class Main extends Component{
             });
         }
     
-        inputMudou= (e) => {
+        handleChange= (e) => {
             this.setState({
                 novaTarefa: e.target.value,
             });
@@ -81,22 +83,18 @@ export default class Main extends Component{
 
         <div className="main">
             <h1>Lista de tarefas</h1>
+            <Form
+                handleSubmit={this.handleSubmit}
+                handleChange={this.handleChange}
+                novaTarefa={novaTarefa}
+            />
+            <Tarefas
+            tarefas={tarefas}
+                handleEdit={this.handleEdit}
+                handleDelete={this.handleDelete}
 
-            <form onSubmit={this.handleSubmit} action="" className="form">
-                <input onChange={this.inputMudou}type="text"
-                value={novaTarefa} />
-                <button type="submit"><FaPlus/></button>
-            </form>
-            <ul className="tarefas">
-                {tarefas.map((tarefa, index) =>(
-                    <li key={tarefa}>{tarefa}
-                    <span>
-                        <FaEdit onClick={(e) =>this.handleEdit(e, index)}className="edit" />
-                        <FaWindowClose onClick={(e) =>this.handleDelete(e, index)} className="delete" />
-                    </span>
-                    </li>
-                ))}
-            </ul>
+            />
+
 
         </div>
         );
